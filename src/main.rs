13561ad2fdcec89de44643e7db3be5681577a136
anaximeno <anaximenobrito@gmail.com>
2022-01-05 22:35:46 -0100

    testing the ball struct

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,20 @@ use raylib::prelude::*;
 
 fn main() {
     let p1 = Player::from(Color::BLUE, Side::LEFT, 32, 60, 200, 200);
-    println!("Hello from p1 at {} pixels of distance from the origin!", p1.dist());
-    println!("p1's height = {}, width = {} and, aceleration = {}", p1.height, p1.width, p1.aceleration);
+
+    println!(
+        "Player 1 says hello from {} pixels of distance of the origin!",
+        p1.dist()
+    );
+    println!(
+        "p1's height = {}, width = {} and, aceleration = {}",
+        p1.height, p1.width, p1.aceleration
+    );
+
+    let b = Ball::from(Color::BEIGE, 15, 15, 369, -125);
+
+    println!(
+        "\nThere is a ball at the distance of {} from the Player 1!",
+        b.dist_from_point(&p1)
+    );
 }
