3323b46516724926e47528039337d571da33ea92
anaximeno <anaximenobrito@gmail.com>
2022-01-05 18:30:56 -0100

    Point struct was abstracted to its module

---
+++ src/point.rs
@@ -0,0 +1,23 @@
+pub struct Point<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl Point<f32> {
+    pub fn dist(&self) -> f32 {
+        self.dist_from(0.0, 0.0)
+    }
+
+    pub fn dist_from(&self, x: f32, y: f32) -> f32 {
+        ((x - self.x).powf(2.0) + (y - self.y).powf(2.0)).sqrt()
+    }
+
+    pub fn dist_from_point(&self, point: &Point<f32>) -> f32 {
+        self.dist_from(point.x, point.y)
+    }
+
+    pub fn theta(&self) -> f32 {
+        (self.y / self.x).atan()
+    }
+}
+
