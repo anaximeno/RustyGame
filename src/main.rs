77b2d01d47e08195e7a25da8e6f96a45928c30b7
anaximeno <anaximenobrito@gmail.com>
2022-01-05 20:12:33 -0100

    point struct was turned into a trait

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod point;
 mod player;
 mod components;
-use components::Point;
+
 
 fn main() {
-    let p = Point{x: 1.5, y: 0.5};
-    println!("Hello, from the distance {} of the axis (0, 0)!", p.dist());
+    println!("Hello, Bourne Shell Again!");
 }
--- src/point.rs
+++ src/point.rs
@@ -1,23 +1,20 @@
-pub struct Point<T> {
-    pub x: T,
-    pub y: T,
-}
+pub trait Point {
+    fn x(&self) -> f32;
+    fn y(&self) -> f32;
 
-impl Point<f32> {
-    pub fn dist(&self) -> f32 {
+    fn dist(&self) -> f32 {
         self.dist_from(0.0, 0.0)
     }
 
-    pub fn dist_from(&self, x: f32, y: f32) -> f32 {
-        ((x - self.x).powf(2.0) + (y - self.y).powf(2.0)).sqrt()
+    fn dist_from(&self, x: f32, y: f32) -> f32 {
+        ((x - self.x()).powf(2.0) + (y - self.y()).powf(2.0)).sqrt()
     }
 
-    pub fn dist_from_point(&self, point: &Point<f32>) -> f32 {
-        self.dist_from(point.x, point.y)
+    fn dist_from_point<T: Point>(&self, point: &T) -> f32 {
+        self.dist_from(point.x(), point.y())
     }
 
-    pub fn theta(&self) -> f32 {
-        (self.y / self.x).atan()
+    fn theta(&self) -> f32 {
+        (self.y() / self.x()).atan()
     }
 }
-
