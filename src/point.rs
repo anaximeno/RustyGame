1946f80c24453ed69c3777773bf8c7bd6d9933bd
anaximeno <anaximenobrito@gmail.com>
2022-01-06 14:33:02 -0100

    add MovablePoint trait
    and also get Point.dist_from method to support generic AsF32 types

--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,6 @@
+use raylib::misc::AsF32;
+
+
 pub trait Point {
     fn x(&self) -> f32;
     fn y(&self) -> f32;
@@ -6,8 +9,10 @@ pub trait Point {
         self.dist_from(0.0, 0.0)
     }
 
-    fn dist_from(&self, x: f32, y: f32) -> f32 {
-        ((x - self.x()).powf(2.0) + (y - self.y()).powf(2.0)).sqrt()
+    fn dist_from<T1: AsF32, T2: AsF32>(&self, x: T1, y: T2) -> f32 {
+        let xx = x.as_f32() - self.x();
+        let yy = y.as_f32() - self.y();
+        (xx.powf(2.0) + yy.powf(2.0)).sqrt()
     }
 
     fn dist_from_point<T: Point>(&self, point: &T) -> f32 {
@@ -16,5 +21,20 @@ pub trait Point {
 
     fn theta(&self) -> f32 {
         (self.y() / self.x()).atan()
+    }
+}
+
+
+pub trait MovablePoint {
+    fn x_ref(&mut self) -> &mut f32;
+    fn y_ref(&mut self) -> &mut f32;
+
+    fn move_to<T1: AsF32, T2: AsF32>(&mut self, x: T1, y: T2) {
+        *self.x_ref() = x.as_f32();
+        *self.y_ref() = y.as_f32();
+    }
+
+    fn move_to_point<T: Point>(&mut self, point: &T) {
+        self.move_to(point.x(), point.y())
     }
 }
