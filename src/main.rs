5b4d79f88476e493a8b80a734a958591db1e6ab5
anaximeno <anaximenobrito@gmail.com>
2022-02-20 00:46:48 -0100

    fix window naming

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ use components::GameWindow;
 
 fn main() {
     use raylib::prelude::Color;
-    let mut game = GameWindow::from("Rusty Block Volley", 720, 480, 60);
+    let mut game = GameWindow::from("Rusty Game", 720, 480, 60);
     game.run_test_window(Color::GRAY);
 }
